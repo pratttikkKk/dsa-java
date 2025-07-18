6cf50541c3cef96d4d5e902d86a9e2b017ca65b7
Pratik Suresh farate <pratikfarate33@gmail.com>
2025-07-18 17:24:49 +0530

    Add SmallestNum.java - find smallest number from product of digits

---
+++ SmallestNum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class SmallestNum {
+	public static void main(String[] args){
+Scanner sc= new Scanner(System.in);
+int n = sc.nextInt();
+System.out.println(solution(n));
+	}
+	
+	 public static int solution(int n){
+		String ans="";
+		for(int i=9; i>2; i--){
+			while(n%i==0){
+				n=n/i;
+				ans=i+ans;
+			}
+		}
+		if(n != 1){
+			return -1;
+		}
+		else{
+			return Integer.parseInt(ans);
+		}
+	}
+}
