c0ee60e52a5392f10cd9c7edaefe3b2949469818
Pratik Suresh farate <pratikfarate33@gmail.com>
2025-09-13 21:10:07 +0530

    13/09/25

---
+++ MajorityElement.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class MajorityElement {
+	public static void main(String[] args) {
+	int[] arr={1,2,3,4,5,6,3,3,3,3,3,3,3,7,8,3};
+	}
+
+	public static int findmajority(int[] arr, int n)
+}
