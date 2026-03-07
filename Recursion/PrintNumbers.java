c6de19dc7d85e51d008577de51d9d29d5a8ccbc9
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-03-07 17:37:27 +0530

    basic recursion

---
+++ Recursion/PrintNumbers.java
@@ -0,0 +1,11 @@
+package Recursion;
+
+public class PrintNumbers{
+	public static void main(String[] args) {
+		function(10);
+	}
+	 public static void function(int n){
+      System.out.println(n);
+	 }
+
+}
