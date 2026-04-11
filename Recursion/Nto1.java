50e50ae80ec8f301aa76bd4c17e5536592de90ab
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-04-11 22:58:24 +0530

    Recursion simple example

--- Recursion/Nto1.java
+++ Recursion/Nto1.java
@@ -1,12 +1,21 @@
 public class Nto1 {
 	public static void main(String[] args) {
-			fun(5,15);
+			fun(5);
+			funRev(5);
 	}
-	static void fun(int s,int e){
-    if(s==e){
+	static void fun(int n){
+    if(n==0){
 			return;
 		}
-		System.out.println(s);
-		fun(s+1,e);
+		System.out.println(n);
+		fun(n-1);
+	}
+	static void funRev(int n){
+    if(n==0){
+			return;
+		}
+		
+		funRev(n-1);
+		System.out.println(n); //just changed position to print numbeers reverse
 	}
 }
