7c39e74d746a04b63dc424417b0c4a165a75ade5
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-04-13 11:06:16 +0530

    Reverse number

--- Recursion/ReverseNumber.java
+++ Recursion/ReverseNumber.java
@@ -2,6 +2,7 @@ public class ReverseNumber {
 
     public static void main(String[] args) {
         reverse(1345);
+        System.out.println("\n"+ReturnReverse(1234, 0));
     }
 
     static void reverse(int n){
@@ -11,5 +12,15 @@ public class ReverseNumber {
 
         System.out.print(n % 10);  // print last digit
         reverse(n / 10);           // remove last digit
+    }
+    static int ReturnReverse(int n,int rev){
+       if(n==0){
+        return rev;
+    }
+    int digit = n%10;
+    rev=rev*10+digit;
+    n=n/10;
+
+    return ReturnReverse(n, rev);
     }
 }
