2324d703e36a56102a7ab3a04ad7908b85f0cde7
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-08-11 19:04:19 +0530

    pattern 4

--- Training_Practice/Prefixsum.java
+++ Training_Practice/Prefixsum.java
@@ -1,5 +1,11 @@
 public class Prefixsum {
 	public static void main(String[] args) {
 		
+		for (int i=4; i>0; i--){
+			for(int j=0; j<i; j++){
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
 	}
 }
