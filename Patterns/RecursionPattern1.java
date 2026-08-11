9b751e3abe598185c0ad6f04a8996c5d13787dbc
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-08-11 19:22:52 +0530

    pattern 4 using recursion

---
+++ Patterns/RecursionPattern1.java
@@ -0,0 +1,21 @@
+public class RecursionPattern1 {
+	public static void main(String[] args) {
+		triangle(10, 0);
+
+	}
+
+	static void triangle(int r, int c){
+     if(r==0){
+			return;
+		 }
+		  if(r>c){
+				System.out.print("*");
+				triangle(r, c+=1);
+			}
+			else{
+				System.out.println();
+				triangle(r-=1, 0);
+			}
+
+	}
+}
