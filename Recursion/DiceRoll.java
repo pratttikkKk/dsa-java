a16dfcc392ce55ed3b9a0eb06f59c010ac1af3ce
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-08-18 23:06:23 +0530

    Add files via upload
    dice code

---
+++ Recursion/DiceRoll.java
@@ -0,0 +1,17 @@
+public class DiceRoll {
+	public static void main(String[] args) {
+		dice("",4);
+	}
+
+	static void dice(String p , int target){
+
+	  if(target == 0){
+       System.out.println(p);
+			 return;
+		}
+
+		for(int i=1; i<=target; i++){
+			dice(p+i, target-i);
+		}
+	}
+}
