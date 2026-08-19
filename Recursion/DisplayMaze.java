dd003bb88b3c72ec7552156aac19e64b94c0498c
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-08-19 23:53:13 +0530

    Add files via upload

--- Recursion/DisplayMaze.java
+++ Recursion/DisplayMaze.java
@@ -4,22 +4,27 @@ import java.util.List;
 public class DisplayMaze {
 	public static void main(String[] args) {
 
-		maze("",3,3);
+		System.out.println(maze("",3,3));
 
 	}
 
-  static void maze(String p,int r, int c){
+  static ArrayList<String> maze(String p,int r, int c){
 
 		if(r==1 && c==1){
-			System.out.println(p);
-			return;
+			ArrayList<String> list = new ArrayList<>();
+       list.add(p);
+			 return list;
 		}
 
+		ArrayList<String> list = new ArrayList<>();
+
 		if(r>1){
-			maze(p+"D", r-1, c);
+			list.addAll(maze(p+"D", r-1, c));
 		}
 		if(c>1){
-			maze(p+"R",r,c-1);
+			list.addAll(maze(p+"R",r,c-1));
 		}
+
+		return list;
 	}
 }
